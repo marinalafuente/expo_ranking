d393c66bbc4e3d3a5a4ea134a9028af9519aab35
Marina <marlafsan@gmail.com>
2014-12-04 13:07:10 +0100

    implementing koala

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.7'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+#gem 'sqlite3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
@@ -41,3 +41,12 @@ gem 'pry'
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin]
 gem 'foundation-rails'
+
+group :development, :test do
+ gem 'sqlite3'
+end
+
+group :production do
+ gem 'pg'
+ gem 'rails_12factor'
+end
--- app/controllers/expos_controller.rb
+++ app/controllers/expos_controller.rb
@@ -1,11 +1,17 @@
 class ExposController < ApplicationController
-		
+before_filter :parse_facebook_cookies
+
+def parse_facebook_cookies
+  @facebook_cookies ||= Koala::Facebook::OAuth.new(YOUR_APP_ID, YOUR_SECRET).get_user_info_from_cookie(cookies)
+end
+
 	def show
 		@expo = Expo.find(params[:id])
 		@voteRequest = Vote.valid?(request.remote_ip, @expo.id)
 	  #Vote.where(ip_address: request.remote_ip).where(expo_id: params[:id]).empty? 		
 		#@voteResult = @expo.votes.where(vote_flag: true).count - @expo.votes.where(vote_flag: false).count 
-					
+		@access_token = @facebook_cookies["access_token"]
+  	@graph = Koala::Facebook::GraphAPI.new(@access_token)		
 	end
 
 		def vote
